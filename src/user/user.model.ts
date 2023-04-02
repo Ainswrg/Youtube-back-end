7fd192a5bd040a3c4028d32658eb2ce88cf0ff10
JaslanD <jaslan.dlnv@gmail.com>
2023-04-02 09:06:11 +0300

    feat: update eslint config

--- src/user/user.model.ts
+++ src/user/user.model.ts
@@ -1,1 +1,33 @@
-export class UserModel {}
+import { prop } from '@typegoose/typegoose'
+import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses'
+
+export interface UserModel extends Base {}
+
+export class UserModel extends TimeStamps {
+  @prop({ unique: true, required: true })
+  email: string
+
+  @prop()
+  password: string
+
+  @prop()
+  name: string
+
+  @prop()
+  isVerified: string
+
+  @prop()
+  subscribersCount: number
+
+  @prop()
+  description: string
+
+  @prop()
+  location: string
+
+  @prop()
+  bannerPath: string
+
+  @prop()
+  avatarPath: string
+}
