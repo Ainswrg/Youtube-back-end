41211bc951f9da6228539b329c78b7b9de200b36
JaslanD <jaslan.dlnv@gmail.com>
2023-04-05 18:17:30 +0300

    feat: implement auth & user

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { JwtModule } from '@nestjs/jwt'
 import { AuthService } from './auth.service'
 import { AuthController } from './auth.controller'
 import { UserModel } from 'src/user/user.model'
 import { TypegooseModule } from 'nestjs-typegoose'
-import { ConfigModule } from '@nestjs/config'
+import { getJWTConfig } from 'src/config/jwt.config'
 
 @Module({
   controllers: [AuthController],
@@ -17,7 +19,12 @@ import { ConfigModule } from '@nestjs/config'
         }
       }
     ]),
-    ConfigModule
+    ConfigModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getJWTConfig
+    })
   ]
 })
 export class AuthModule {}
